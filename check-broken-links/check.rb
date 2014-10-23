2199cc7359faac79da883d00a32777ad85a6d7d9
Marco Santos <me@marcosantos.me>
2014-10-23 11:03:09 +0100

    check-broken-links: added script to find URIs with HTTP code != 200

---
+++ check-broken-links/check.rb
@@ -0,0 +1,48 @@
+require 'nokogiri'
+require 'open-uri'
+require 'uri'
+
+if ARGV.length != 1
+  puts "Usage: ruby #{__FILE__} URI"
+  exit 1
+end
+
+begin
+  page = Nokogiri::HTML open ARGV[0]
+  
+  links = []
+  
+  page.css('a').each do |anchor|
+    
+    if !anchor['href'].nil? and !anchor['href'].empty?
+      
+      href = anchor['href'].sub(ARGV[0], '')
+      href.slice!(/(.*)(#.*)/, 1)
+      
+      if !links.include? href and href.match '^/.*$'
+        links << href
+      end
+    end
+    
+  end
+  
+  uri = URI.parse ARGV[0]
+  
+  links.each do |link|
+    Net::HTTP.start(uri.host, uri.port) do |http|
+      
+      response = http.head link
+      
+      if response.code != '0'
+        puts "#{response.code} #{uri.scheme}://#{uri.host}#{link}"
+      end
+      
+    end
+  end
+  
+rescue SocketError => e
+  puts "Could not open #{ARGV[0]} (#{e.message})"
+  exit 2
+end
+
+exit 0
